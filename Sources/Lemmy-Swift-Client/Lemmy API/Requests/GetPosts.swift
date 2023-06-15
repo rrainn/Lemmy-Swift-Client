26dfa3d889769ec27babe88fd9bc7083f188ad62
Charlie Fish <fishcharlie@me.com>
2023-06-15 10:49:42 -0400

    Adding better documentation for GetPosts `limit` parameter

--- Sources/Lemmy-Swift-Client/Lemmy API/Requests/GetPosts.swift
+++ Sources/Lemmy-Swift-Client/Lemmy API/Requests/GetPosts.swift
@@ -17,6 +17,11 @@ public struct GetPostsRequest: APIRequest {
 	public let community_id: Int?
 	/// To get posts for a federated community by name, use `name@instance.tld`.
 	public let community_name: String?
+	/// The maximum number of posts to retrieve.
+	///
+	/// It is possible that less posts will be returned if the maximum is greater than the number of posts available.
+	///
+	/// The server will throw a `couldnt_get_posts` error if you pass in a limit greater than 50.
 	public let limit: Int?
 	public let page: Int?
 	public let saved_only: Bool?
