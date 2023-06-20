045f99e2f869a7aa7da854e4076972702a865354
Michael Higgins <mdhiggins23@gmail.com>
2023-06-20 09:56:25 -0400

    indent fix

--- Sources/Lemmy-Swift-Client/Lemmy API/Enums/SortType.swift
+++ Sources/Lemmy-Swift-Client/Lemmy API/Enums/SortType.swift
@@ -9,22 +9,22 @@ import Foundation
 
 /// Different post sort types used in lemmy.
 public enum SortType: String, Codable, CustomStringConvertible, CaseIterable {
-	/// Posts sorted by hot, but bumped by new comments up to 2 days.
-	case active = "Active"
-	/// Posts sorted by a decaying rank.
-	case hot = "Hot"
+    /// Posts sorted by hot, but bumped by new comments up to 2 days.
+    case active = "Active"
+    /// Posts sorted by a decaying rank.
+    case hot = "Hot"
     /// Posts sorted by the published time.
     case new = "New"
-	/// Posts sorted by the published time ascending
-	case old = "Old"
+    /// Posts sorted by the published time ascending
+    case old = "Old"
     /// The top posts for this last day.
     case topDay = "TopDay"
     /// The top posts for this last week.
     case topWeek = "TopWeek"
-	/// The top posts for this last month.
-	case topMonth = "TopMonth"
-	/// The top posts for this last year.
-	case topYear = "TopYear"
+    /// The top posts for this last month.
+    case topMonth = "TopMonth"
+    /// The top posts for this last year.
+    case topYear = "TopYear"
     /// The top posts of all time.
     case topAll = "TopAll"
     /// Posts sorted by the most comments.
