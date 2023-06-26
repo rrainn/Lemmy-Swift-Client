3529750e9408d866ed7da88c087d15d051c466b6
Michael Higgins <mdhiggins23@gmail.com>
2023-06-26 01:48:05 -0400

    Revert "saveable protocol"
    This reverts commit b6f70029575e60fab192dff4db5937cf8a9fa081.

--- Sources/Lemmy-Swift-Client/Lemmy API/Requests/GetCommunity.swift
+++ Sources/Lemmy-Swift-Client/Lemmy API/Requests/GetCommunity.swift
@@ -28,15 +28,15 @@ public struct GetCommunityResponse: APIResponse {
 	public let default_post_language: Int?
 	public let discussion_languages: [Int]
 	public let moderators: [CommunityModeratorView]
-//	public let online: Int
+	public let online: Int
 	public let site: Site?
 
-	public init(community_view: CommunityView, default_post_language: Int? = nil, discussion_languages: [Int], moderators: [CommunityModeratorView], site: Site? = nil) {
+	public init(community_view: CommunityView, default_post_language: Int? = nil, discussion_languages: [Int], moderators: [CommunityModeratorView], online: Int, site: Site? = nil) {
 		self.community_view = community_view
 		self.default_post_language = default_post_language
 		self.discussion_languages = discussion_languages
 		self.moderators = moderators
-//		self.online = online
+		self.online = online
 		self.site = site
 	}
 }
