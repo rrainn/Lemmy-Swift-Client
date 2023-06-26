1370b839da94418eeda59ded32ee63a29ba8d458
Michael Higgins <mdhiggins23@gmail.com>
2023-06-26 01:49:03 -0400

    make online optional
    0.18 compatability

--- Sources/Lemmy-Swift-Client/Lemmy API/Requests/GetCommunity.swift
+++ Sources/Lemmy-Swift-Client/Lemmy API/Requests/GetCommunity.swift
@@ -28,10 +28,10 @@ public struct GetCommunityResponse: APIResponse {
 	public let default_post_language: Int?
 	public let discussion_languages: [Int]
 	public let moderators: [CommunityModeratorView]
-	public let online: Int
+	public let online: Int?
 	public let site: Site?
 
-	public init(community_view: CommunityView, default_post_language: Int? = nil, discussion_languages: [Int], moderators: [CommunityModeratorView], online: Int, site: Site? = nil) {
+	public init(community_view: CommunityView, default_post_language: Int? = nil, discussion_languages: [Int], moderators: [CommunityModeratorView], online: Int?, site: Site? = nil) {
 		self.community_view = community_view
 		self.default_post_language = default_post_language
 		self.discussion_languages = discussion_languages
