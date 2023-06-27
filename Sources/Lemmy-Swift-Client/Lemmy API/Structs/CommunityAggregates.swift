1bc02fa345d6444c19f7d09cfd9c477049f25dae
Michael Higgins <mdhiggins23@gmail.com>
2023-06-27 07:36:04 -0400

    spacing fixes

--- Sources/Lemmy-Swift-Client/Lemmy API/Structs/CommunityAggregates.swift
+++ Sources/Lemmy-Swift-Client/Lemmy API/Structs/CommunityAggregates.swift
@@ -17,10 +17,10 @@ public struct CommunityAggregates: Codable {
 	public let users_active_half_year: Int
 	public let users_active_month: Int
 	public let users_active_week: Int
-    public let published: Date
-    public let hot_rank: Int
+	public let published: Date
+	public let hot_rank: Int
 
-    public init(comments: Int, community_id: Int, id: Int, posts: Int, subscribers: Int, users_active_day: Int, users_active_half_year: Int, users_active_month: Int, users_active_week: Int, published: Date, hot_rank: Int) {
+	public init(comments: Int, community_id: Int, id: Int, posts: Int, subscribers: Int, users_active_day: Int, users_active_half_year: Int, users_active_month: Int, users_active_week: Int, published: Date, hot_rank: Int) {
 		self.comments = comments
 		self.community_id = community_id
 		self.id = id
@@ -30,7 +30,7 @@ public struct CommunityAggregates: Codable {
 		self.users_active_half_year = users_active_half_year
 		self.users_active_month = users_active_month
 		self.users_active_week = users_active_week
-        self.published = published
-        self.hot_rank = hot_rank
+		self.published = published
+		self.hot_rank = hot_rank
 	}
 }
