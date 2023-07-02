73c0f65d3f4e9eab4e3a22a67002c69712a9b0ac
Charlie Fish <fishcharlie@me.com>
2023-07-01 22:10:12 -0600

    Linting code

--- Sources/Lemmy-Swift-Client/Lemmy_Swift_Client.swift
+++ Sources/Lemmy-Swift-Client/Lemmy_Swift_Client.swift
@@ -12,31 +12,35 @@ public class LemmyAPI {
 		self.urlSession = urlSession
 	}
 
-    /// Do request to lemmy server and return URLSession result
-    public func baseRequest<T: APIRequest>(_ apiRequest: T) async throws -> (T.Response, URLResponse, Data) {
-        var request = URLRequest(url: baseUrl.appending(path: T.path))
-        request.httpMethod = T.httpMethod.rawValue
-        let encoder = JSONEncoder()
-        if T.httpMethod == .get {
-            let mirror = Mirror(reflecting: apiRequest)
-            request.url = request.url?.appending(queryItems: mirror.children.compactMap { (label, value) in
-                guard let label, let valueString = value as? CustomStringConvertible else { return nil }
-                
-                return URLQueryItem(name: label, value: String(describing: valueString))
-            })
-        } else {
-            request.httpBody = try encoder.encode(apiRequest)
-        }
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        let (data, response) = try await URLSession.shared.data(for: request)
-        let decoder = JSONDecoder()
-        let decodedResult = try decoder.decode(T.Response.self, from: data)
-        return (decodedResult, response, data)
-    }
-    
+	/// Do request to lemmy server and return URLSession result
+	public func baseRequest<T: APIRequest>(_ apiRequest: T) async throws
+		-> (T.Response, URLResponse, Data)
+	{
+		var request = URLRequest(url: baseUrl.appending(path: T.path))
+		request.httpMethod = T.httpMethod.rawValue
+		let encoder = JSONEncoder()
+		if T.httpMethod == .get {
+			let mirror = Mirror(reflecting: apiRequest)
+			request.url = request.url?
+				.appending(queryItems: mirror.children.compactMap { label, value in
+					guard let label,
+					      let valueString = value as? CustomStringConvertible else { return nil }
+
+					return URLQueryItem(name: label, value: String(describing: valueString))
+				})
+		} else {
+			request.httpBody = try encoder.encode(apiRequest)
+		}
+		request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+		let (data, response) = try await URLSession.shared.data(for: request)
+		let decoder = JSONDecoder()
+		let decodedResult = try decoder.decode(T.Response.self, from: data)
+		return (decodedResult, response, data)
+	}
+
 	public func request<T: APIRequest>(_ apiRequest: T) async throws -> T.Response {
-        let (result, _, _) = try await baseRequest(apiRequest);
-        return result
+		let (result, _, _) = try await baseRequest(apiRequest)
+		return result
 	}
 }
 
