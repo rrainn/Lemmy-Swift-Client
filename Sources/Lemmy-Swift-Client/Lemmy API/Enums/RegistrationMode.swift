a8c8cebd7bdbaf0298a63d29c19889c99d504372
Charlie Fish <fishcharlie@me.com>
2023-07-03 14:22:49 -0600

    Fixing RegistrationMode (#33)

--- Sources/Lemmy-Swift-Client/Lemmy API/Enums/RegistrationMode.swift
+++ Sources/Lemmy-Swift-Client/Lemmy API/Enums/RegistrationMode.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 public enum RegistrationMode: String, Codable, CustomStringConvertible, CaseIterable {
-	case closed
-	case open
+	case closed = "Closed"
+	case open = "Open"
 	case requireApplication = "RequireApplication"
 
 	public var description: String {
