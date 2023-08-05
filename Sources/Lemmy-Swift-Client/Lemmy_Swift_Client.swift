304430b1954f3ff62810d0931293fcd069d11c83
Charlie Fish <fishcharlie@me.com>
2023-08-05 10:53:47 -0600

    Adding error handling

--- Sources/Lemmy-Swift-Client/Lemmy_Swift_Client.swift
+++ Sources/Lemmy-Swift-Client/Lemmy_Swift_Client.swift
@@ -1,5 +1,24 @@
 import Foundation
 
+public enum LemmyAPIError: Error, CustomStringConvertible, LocalizedError {
+	case badResponse(response: URLResponse, request: URLRequest, data: Data)
+	case invalidResponse(response: HTTPURLResponse, request: URLRequest, data: Data)
+	case decodeError(_ error: Error, data: Data, request: URLRequest)
+
+	public var description: String {
+		switch self {
+		case .badResponse(response: _, request: _, data: _):
+			return "Bad response from server"
+		case .invalidResponse(response: let response, request: _, data: _):
+			return "Invalid response from server, status code \(response.statusCode)"
+		case .decodeError(let error, data: _, request: _):
+			return "Decode error, \(error.localizedDescription) (\(error))"
+		}
+	}
+
+	public var errorDescription: String? { return description }
+}
+
 /// An instance of the Lemmy API.
 public class LemmyAPI {
 	public let baseUrl: URL
@@ -36,11 +55,26 @@ public class LemmyAPI {
 	public func baseRequest<T: APIRequest>(_ apiRequest: T) async throws
 		-> (T.Response, URLResponse, Data)
 	{
+		// Make Request
 		let request = try urlRequest(apiRequest)
-		let (data, response) = try await URLSession.shared.data(for: request)
+		let (data, response) = try await urlSession.data(for: request)
+		if let urlResponse = response as? HTTPURLResponse,
+		   urlResponse.statusCode != 200 {
+			throw LemmyAPIError.badResponse(
+				response: urlResponse,
+				request: request,
+				data: data
+			)
+		}
+
+		// Decode Response
 		let decoder = JSONDecoder()
-		let decodedResult = try decoder.decode(T.Response.self, from: data)
-		return (decodedResult, response, data)
+		do {
+			let decodedResult = try decoder.decode(T.Response.self, from: data)
+			return (decodedResult, response, data)
+		} catch {
+			throw LemmyAPIError.decodeError(error, data: data, request: request)
+		}
 	}
 
 	public func request<T: APIRequest>(_ apiRequest: T) async throws -> T.Response {
