00405f2e195a326486c25da6381d674344629141
Colin Walsh <colinfwalsh@gmail.com>
2023-12-06 23:57:41 -0500

    Fix codable model

--- Sources/Lemmy-Swift-Client/Lemmy API/Structs/PostView.swift
+++ Sources/Lemmy-Swift-Client/Lemmy API/Structs/PostView.swift
@@ -11,6 +11,7 @@ public struct PostView: Codable, Hashable {
 	public let read: Bool
 	public let creator_blocked: Bool
 	public let my_vote: Int?
+    public let featured_community: Bool
 	public let unread_comments: Int
 
 	public init(
@@ -23,6 +24,7 @@ public struct PostView: Codable, Hashable {
 		saved: Bool,
 		read: Bool,
 		creator_blocked: Bool,
+        featured_community: Bool,
 		my_vote: Int? = nil,
 		unread_comments: Int
 	) {
@@ -36,6 +38,7 @@ public struct PostView: Codable, Hashable {
 		self.read = read
 		self.creator_blocked = creator_blocked
 		self.my_vote = my_vote
+        self.featured_community = featured_community
 		self.unread_comments = unread_comments
 	}
 }
