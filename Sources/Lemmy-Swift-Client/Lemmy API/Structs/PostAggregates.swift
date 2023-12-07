aaf685ea9b7eea4ee1671e8b834d80f01da3878e
Colin Walsh <colinfwalsh@gmail.com>
2023-12-07 00:15:24 -0500

    Fix aggregates

--- Sources/Lemmy-Swift-Client/Lemmy API/Structs/PostAggregates.swift
+++ Sources/Lemmy-Swift-Client/Lemmy API/Structs/PostAggregates.swift
@@ -11,12 +11,6 @@ public struct PostAggregates: Codable, Identifiable, Hashable {
 	public let upvotes: Int
 	public let downvotes: Int
 	public let published: String
-	public let newest_comment_time_necro: String
-	public let newest_comment_time: String
-	public let featured_community: Bool
-	public let featured_local: Bool
-	public let hot_rank: Int
-	public let hot_rank_active: Int
 
 	public init(
 		post_id: PostId,
@@ -24,13 +18,7 @@ public struct PostAggregates: Codable, Identifiable, Hashable {
 		score: Int,
 		upvotes: Int,
 		downvotes: Int,
-		published: String,
-		newest_comment_time_necro: String,
-		newest_comment_time: String,
-		featured_community: Bool,
-		featured_local: Bool,
-		hot_rank: Int,
-		hot_rank_active: Int
+		published: String
 	) {
 		self.post_id = post_id
 		self.comments = comments
@@ -38,11 +26,5 @@ public struct PostAggregates: Codable, Identifiable, Hashable {
 		self.upvotes = upvotes
 		self.downvotes = downvotes
 		self.published = published
-		self.newest_comment_time_necro = newest_comment_time_necro
-		self.newest_comment_time = newest_comment_time
-		self.featured_community = featured_community
-		self.featured_local = featured_local
-		self.hot_rank = hot_rank
-		self.hot_rank_active = hot_rank_active
 	}
 }
