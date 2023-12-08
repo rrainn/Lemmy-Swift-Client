1e59e013e0c3f5d02aa8162eaeaa629fa3a3f74b
Colin Walsh <colinfwalsh@gmail.com>
2023-12-08 15:10:56 -0500

    Fixed incorrect call

--- Sources/Lemmy-Swift-Client/Lemmy_Swift_Client.swift
+++ Sources/Lemmy-Swift-Client/Lemmy_Swift_Client.swift
@@ -125,7 +125,7 @@ extension String {
         let pattern = "([a-z0-9])([A-Z])"
         
         let regex = try? NSRegularExpression(pattern: pattern, options: [])
-        let range = NSRange(location: 0, length: self.characters.count)
+        let range = NSRange(location: 0, length: self.count)
         return regex?.stringByReplacingMatches(in: self, options: [], range: range, withTemplate: "$1_$2").lowercased()
     }
 }
