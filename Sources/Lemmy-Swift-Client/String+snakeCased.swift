a97edc2b61d8d7308cb23a588ec19969d7fabe55
Colin Walsh <colinfwalsh@gmail.com>
2023-12-08 18:40:38 -0500

    Lint fix

--- Sources/Lemmy-Swift-Client/String+snakeCased.swift
+++ Sources/Lemmy-Swift-Client/String+snakeCased.swift
@@ -1,23 +1,16 @@
-//
-//  File.swift
-//  
-//
-//  Created by Colin Walsh on 12/8/23.
-//
-
 import Foundation
 
 extension String {
-    func snakeCased() -> String? {
-        let pattern = "([a-z0-9])([A-Z])"
+	func snakeCased() -> String? {
+		let pattern = "([a-z0-9])([A-Z])"
 
-        let regex = try? NSRegularExpression(pattern: pattern, options: [])
-        let range = NSRange(location: 0, length: count)
-        return regex?.stringByReplacingMatches(
-            in: self,
-            options: [],
-            range: range,
-            withTemplate: "$1_$2"
-        ).lowercased()
-    }
+		let regex = try? NSRegularExpression(pattern: pattern, options: [])
+		let range = NSRange(location: 0, length: count)
+		return regex?.stringByReplacingMatches(
+			in: self,
+			options: [],
+			range: range,
+			withTemplate: "$1_$2"
+		).lowercased()
+	}
 }
